ff0b6785eb209455783078dc97e1fc6174a657f2
Janet Guo <junzhu.guo@accenture.com>
2019-02-26 13:09:21 -0500

    inital commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+setup(
+    name='paragrammer',
+    version='0.1dev',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'matplotlib~=3.0.2'
+    ]
+)
