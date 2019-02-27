d60ab0bfddce82d497ccf9c2c815ca77fde9919e
Janet Guo <junzhu.guo@accenture.com>
2019-02-27 08:18:56 -0500

    draw list of points

--- draw.py
+++ draw.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 
 from src.parallelogram import *
 
-def Draw():
+class Draw():
     @staticmethod
-    def get_box_coordinates(box: Parallelogram):
+    def get_box_coordinates(box):
         x_coords = [box.p1.x, box.p2.x, box.p3.x, box.p4.x, box.p1.x]
         y_coords = [box.p1.y, box.p2.y, box.p3.y, box.p4.y, box.p1.y]
         return x_coords, y_coords
         
     @staticmethod
-    def show():
+    def show(gram_ls, title="Bounding Boxes"):
         # Set chart title.
-        plt.title("Bounding Boxes", fontsize=19)
+        plt.title(title, fontsize=19)
 
         # Set x axis label.
         plt.xlabel("x-coordinates", fontsize=10)
@@ -23,13 +23,23 @@ def Draw():
         # Set size of tick labels.
         plt.tick_params(axis='both', which='major', labelsize=9)
 
+        # plot
+        for gram in gram_ls:
+            x_ls, y_ls = Draw().get_box_coordinates(gram)
+            plt.plot(x_ls, y_ls, linewidth=3)
+
         # Display the plot in the matplotlib's viewer.
         plt.show()
 
 if __name__ == '__main__':
     points = [Point(0, 0), Point(5,10)]
-    print(points[0] + points[1])
-    # Parallelogram(points)
+    # print(points[0] + points[1])
+    
+    para = Parallelogram(points)
+    print("width: {}, height: {}".format(para.get_width(), para.get_height()))
+    gram_ls = [para]
+    print(para)
+    Draw.show(gram_ls)
     #################################
     # p1 = Point(0, 0)
     # p2 = Point(5, 5)
